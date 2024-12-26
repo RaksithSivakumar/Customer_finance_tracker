4ce14cce55fff3e4ab9e39277882e72a5703a2e6
RaksithSivakumar <devrisi78@gmail.com>
2024-12-26 19:50:42 +0530

    Some Changes related to the path

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,39 +1,36 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom';
-import LoginPage from './LoginPage';
-import Dashboard from './Dashboard';
-import TotalCustomers from './TotalCustomers';
-import CustomerDetails from './CustomerDetails';
-import AddCustomer from './AddCustomer';
-import PreviewCustomer from './PreviewCustomer';
-import Menu from './Menu';
+import LoginPage from './LoginPage/LoginPage';  // Ensure the correct path
+import Dashboard from './Dashboard/Dashboard';
+import TotalCustomers from './TotalCustomers/TotalCustomers';
+import CustomerDetails from './CustomerDetails/CustomerDetails';
+import AddCustomer from './AddCustomer/AddCustomer';
+import PreviewCustomer from './PreviewCustomer/PreviewCustomer';
+import Menu from './Menu/Menu';
+
 
 function App() {
-  const [customers, setCustomers] = useState([]);
-
-  const addCustomer = (customer) => {
-    setCustomers((prevCustomers) => [...prevCustomers, customer]);
-  };
-
   return (
     <BrowserRouter>
-      <AppContent addCustomer={addCustomer} customers={customers} />
+      <AppContent />
     </BrowserRouter>
   );
 }
 
-function AppContent({ addCustomer, customers }) {
+function AppContent() {
   const location = useLocation();
 
   return (
     <div className="App">
+      {/* Only show Menu if the current route is not "/" */}
       {location.pathname !== '/' && <Menu />}
+
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/total-customers" element={<TotalCustomers customers={customers} />} />
+        <Route path="/total-customers" element={<TotalCustomers />} />
         <Route path="/customer-details" element={<CustomerDetails />} />
-        <Route path="/add-customer" element={<AddCustomer addCustomer={addCustomer} />} />
+        <Route path="/add-customer" element={<AddCustomer />} />
         <Route path="/preview-customer" element={<PreviewCustomer />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
